20c5abe6a691039a3ac76c860a2b074f38607c15
이춘호 <lch92458@gmail.com>
2025-07-25 21:12:36 +0900

    이미지 로드되지 않은경우 블러처리

--- src/components/mobileImage/mobileImage.tsx
+++ src/components/mobileImage/mobileImage.tsx
@@ -1,4 +1,5 @@
-import styles from './mobileImage.module.scss';
+import { useState } from "react";
+import styles from "./mobileImage.module.scss";
 
 interface MobileImageProps {
   src: string;
@@ -7,9 +8,17 @@ interface MobileImageProps {
 }
 
 export default function MobileImage({ src, alt, className }: MobileImageProps) {
+  const [isLoaded, setIsLoaded] = useState(false);
+
   return (
     <div className={`${styles.wrapper} ${className}`}>
-      <img src={src} alt={alt} loading="lazy" className={styles.image} />
+      <img
+        src={src}
+        alt={alt}
+        loading="lazy"
+        className={`${styles.image} ${isLoaded ? styles.loaded : styles.loading}`}
+        onLoad={() => setIsLoaded(true)}
+      />
     </div>
   );
 }
